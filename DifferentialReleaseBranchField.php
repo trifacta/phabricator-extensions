9e509671a0a79ea674e3f14595b7cf2f60e0e9b8
Seshadri Mahalingam <sesh@trifacta.com>
2015-06-10 10:06:11 -0700

    Adds a "Release Branch" field to Differential
    Summary:
    Our very first custom differential field, complete with Herald support. See
    https://secure.phabricator.com/book/phabricator/article/custom_fields/ for more
    information about custom fields in Phabricator.
    
    Over time we can improve on this; we can add a field for the corresponding dev
    version of the revision, we can add validation for this field to ensure the
    branch always exists, etc. For now, I'd like to keep the first cut of this as
    straightforward as possible.
    
    Test Plan:
    Start up a test Phabricator instance for development. To no one's surprise, I
    used a docker image to do this locally. See https://phab.trifacta.com/P38 for
    the details.
    
    Add the php file from this diff to the `phabricator/src/extensions/`
    folder. Note that this folder is gitignore'd, so future upgrades of Phabricator
    should remain seamless.
    
    Set up a test repository (find some old github repo you don't really care about
    any more (you don't even have to give Phabricator write access to it)) and try
    creating a diff for it. You'll see the "Release Branch" field show up when you
    `arc diff`.
    
    Add a Herald rule to take some action when the release branch matches some
    regexp. Try out your Herald rule by creating the differential revision.
    
    Reviewers: tan, arash, #tf-arc, zasgar, vihang
    
    Reviewed By: #tf-arc, zasgar, vihang
    
    Subscribers: alexras
    
    Differential Revision: https://phab.trifacta.com/D4969

---
+++ DifferentialReleaseBranchField.php
@@ -0,0 +1,191 @@
+<?php
+
+final class DifferentialReleaseBranchField
+  extends DifferentialStoredCustomField {
+
+  public function getFieldType() {
+    return 'text';
+  }
+
+  public function getFieldKey() {
+    return 'trifacta:release-branch';
+  }
+
+  public function getFieldKeyForConduit() {
+    return 'releaseRevision';
+  }
+
+  public function getFieldName() {
+    return pht('Release Branch');
+  }
+
+  public function getFieldDescription() {
+    return pht('Indicates the intended branch for this revision.');
+  }
+
+  public function getHeraldFieldValue() {
+    return $this->getValue();
+  }
+
+  public function shouldDisableByDefault() {
+    return false;
+  }
+
+  public function shouldAppearInPropertyView() {
+    return true;
+  }
+
+  public function renderPropertyViewLabel() {
+    return $this->getFieldName();
+  }
+
+  public function renderPropertyViewValue(array $handles) {
+    return $this->getValue();
+  }
+
+  public function shouldAppearInEditView() {
+    return true;
+  }
+
+  public function shouldAppearInApplicationTransactions() {
+    return true;
+  }
+
+  public function shouldAppearInCommitMessageTemplate() {
+    return true;
+  }
+
+  public function getOldValueForApplicationTransactions() {
+    return $this->getValue();
+  }
+
+  public function getNewValueForApplicationTransactions() {
+    return $this->getValue();
+  }
+
+  public function readValueFromRequest(AphrontRequest $request) {
+    $this->setValue($request->getStr($this->getFieldKey()));
+  }
+
+  public function renderEditControl(array $handles) {
+    return id(new AphrontFormTextControl())
+      ->setName($this->getFieldKey())
+      ->setValue($this->getValue())
+      ->setLabel($this->getFieldName());
+  }
+
+  public function getApplicationTransactionTitle(
+    PhabricatorApplicationTransaction $xaction) {
+    $author_phid = $xaction->getAuthorPHID();
+    $old = $xaction->getOldValue();
+    $new = $xaction->getNewValue();
+
+    return pht(
+      '%s updated the release branch for this revision.',
+      $xaction->renderHandleLink($author_phid));
+  }
+
+  public function getApplicationTransactionTitleForFeed(
+    PhabricatorApplicationTransaction $xaction) {
+
+    $object_phid = $xaction->getObjectPHID();
+    $author_phid = $xaction->getAuthorPHID();
+    $old = $xaction->getOldValue();
+    $new = $xaction->getNewValue();
+
+    return pht(
+      '%s updated the release branch for %s.',
+      $xaction->renderHandleLink($author_phid),
+      $xaction->renderHandleLink($object_phid));
+  }
+
+  public function shouldAppearInCommitMessage() {
+    return true;
+  }
+
+  public function shouldAllowEditInCommitMessage() {
+    return true;
+  }
+
+  public function shouldOverwriteWhenCommitMessageIsEdited() {
+    return true;
+  }
+
+  public function getCommitMessageLabels() {
+    return array(
+      'Release Branch'
+    );
+  }
+
+  public function renderCommitMessageValue(array $handles) {
+    return $this->getValue();
+  }
+
+  public function shouldAppearInConduitDictionary() {
+    return true;
+  }
+
+  // Copied from phabricator/src/infrastructure/customfield/standard/PhabricatorStandardCustomFieldText.php
+
+  public function buildFieldIndexes() {
+    $indexes = array();
+
+    $value = $this->getFieldValue();
+    if (strlen($value)) {
+      $indexes[] = $this->newStringIndex($value);
+    }
+
+    return $indexes;
+  }
+
+  public function readApplicationSearchValueFromRequest(
+    PhabricatorApplicationSearchEngine $engine,
+    AphrontRequest $request) {
+
+    return $request->getStr($this->getFieldKey());
+  }
+
+  public function applyApplicationSearchConstraintToQuery(
+    PhabricatorApplicationSearchEngine $engine,
+    PhabricatorCursorPagedPolicyAwareQuery $query,
+    $value) {
+
+    if (strlen($value)) {
+      $query->withApplicationSearchContainsConstraint(
+        $this->newStringIndex(null),
+        $value);
+    }
+  }
+
+  public function appendToApplicationSearchForm(
+    PhabricatorApplicationSearchEngine $engine,
+    AphrontFormView $form,
+    $value) {
+
+    $form->appendChild(
+      id(new AphrontFormTextControl())
+        ->setLabel($this->getFieldName())
+        ->setName($this->getFieldKey())
+        ->setValue($value));
+  }
+
+  public function shouldAppearInHerald() {
+    return true;
+  }
+
+  public function getHeraldFieldConditions() {
+    return array(
+      HeraldAdapter::CONDITION_CONTAINS,
+      HeraldAdapter::CONDITION_NOT_CONTAINS,
+      HeraldAdapter::CONDITION_IS,
+      HeraldAdapter::CONDITION_IS_NOT,
+      HeraldAdapter::CONDITION_REGEXP,
+    );
+  }
+
+  public function readValueFromCommitMessage($value) {
+    $this->setValue($value);
+    return $this;
+  }
+
+}
